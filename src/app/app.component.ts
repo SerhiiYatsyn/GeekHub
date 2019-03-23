4824e18f064952b8f07f766c9ac50c71371ea105
SerhiiYatsyn <44030464+SerhiiYatsyn@users.noreply.github.com>
2019-03-23 19:35:39 +0200

    Added HW14 (lvl 2 done)

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { Task } from './modules/Task';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  Tasks: Task[] = [];
+  EditMode = false;
+  DoneCount = 0;
+  UnDoneCount = 0;
+  selected = 'all';
+  titleOfTask: string;
+  tempTask: Task;
+
+
+
+
+  filterAll(selected) {
+    this.Tasks.forEach(t => t.hide = false);
+    switch (selected) {
+      case 'all': break;
+      case 'done':
+        this.Tasks.forEach(t => {if(!t.complete) t.hide = true; });
+        break;
+      case 'undone':
+        this.Tasks.forEach(t => {if(t.complete) t.hide = true; });
+        break;
+      case  'archived':
+        this.Tasks.forEach((t => {if(!t.archived) t.hide = true; }));
+    }
+  }
+
+
+  updates(eventParam) {
+    switch (eventParam[1]) {
+      case 'addTask':
+        this.UnDoneCount++;
+        this.Tasks = eventParam[2];
+        break;
+      case 'perform':
+        if(eventParam[2] === 'complete') {
+          console.log(eventParam);
+          this.DoneCount++;
+          this.UnDoneCount--;
+        }
+        else if (eventParam[2] === 'uncomplete') {
+          console.log(eventParam);
+
+          this.DoneCount--;
+          this.UnDoneCount++;
+        }
+        break;
+      case 'editMode':
+        this.EditMode = true;
+        this.titleOfTask = eventParam[2];
+        this.tempTask = eventParam[3];
+        console.log(eventParam, 'upper');
+        console.log(this.titleOfTask, 'upper2');
+        break;
+      case 'editDone':
+        this.EditMode = false;
+        break;
+      case 'delete':
+        if(eventParam[2] === 'done') this.DoneCount--;
+        else if(eventParam[2] === 'undone') this.UnDoneCount--;
+        break;
+    }
+  }
+}
---
+++ src/app/modules/Task.ts
@@ -0,0 +1,8 @@
+export interface Task {
+  id: number;
+  title: string;
+  complete: boolean;
+  hide: boolean;
+  archived: boolean;
+  deleted: boolean;
+}
